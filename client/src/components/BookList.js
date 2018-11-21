adeb213ac6eec7d0004d9eab645de4c35f6da361
Vu-John <johnvu1995@gmail.com>
2018-11-21 13:59:39 -0500

    Render book list

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,11 +12,24 @@ const getBooksQuery = gql`
 `
 
 class BookList extends Component {
+  renderBooks() {
+    var data = this.props.data;
+    if(data.loading) {
+      return (<div>Loading books...</div>);
+    } else {
+      return data.books.map((book) => {
+        return (
+          <li key={book.id}>{book.name}</li>
+        );
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <ul id="book-list">
-            <li>TEST</li>
+          {this.renderBooks()}
         </ul>
       </div>
     );
