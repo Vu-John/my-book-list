a2639cfe885bcb17fdfb04e9f77e11a0e4684134
Vu-John <johnvu1995@gmail.com>
2018-11-21 19:24:37 -0500

    Add app.js with env variables

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo'; // glue query to component
 import { getBookQuery } from '../queries/queries'
-import BookList from './BookList';
 
 class BookDetails extends Component {
   renderBookDetails = () => {
